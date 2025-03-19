798327767b241c7b8447ebe7e988899aba94fbbe
Ian Ellison-Taylor <IanET@users.noreply.github.com>
2025-03-18 17:49:05 -0700

    ...

--- src/LibBaseTsd.jl
+++ src/LibBaseTsd.jl
@@ -446,7 +446,6 @@ macro L_str(s) Base.cconvert(Cwstring, s) end
 # Export all
 for name in names(@__MODULE__; all=true)
     if name in [:eval, :include, Symbol("#eval"), Symbol("#include")]; continue end
-    @show name
     @eval export $name
 end
 
